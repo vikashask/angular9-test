45096217942ae8b44ca72bce615eaee97ca84174
vikash <vikashraj144@gmail.com>
2020-06-26 07:10:59 +0530

    app module

--- README.md
+++ README.md
@@ -25,3 +25,11 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+1> creating lazy loading module
+ng generate module page1 --route page1 --module app.module
+ng generate module page2 --route page2 --module app.module
+ng generate module page3 --route page3 --module app.module
+ng generate module page4 --route page4 --module app.module
+ng generate module page5 --route page5 --module app.module
+ng generate module page6 --route page6 --module app.module
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page1Module } from './page1/page1.module';
+import { Page2Module } from './page2/page2.module';
+import { Page3Module } from './page3/page3.module';
+import { Page4Module } from './page4/page4.module';
+import { Page5Module } from './page5/page5.module';
+import { Page6Module } from './page6/page6.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Page1Module,
+    Page2Module,
+    Page3Module,
+    Page4Module,
+    Page5Module,
+    Page6Module
   ],
   providers: [],
   bootstrap: [AppComponent]
